08bc5259d405e2d360a564cae72d5e8cd630b05f
David Sanderson <dasanderson@deloitte.com>
2021-09-23 23:57:34 -0400

    working todo list

--- src/app/features/TodoList/TodoList.js
+++ src/app/features/TodoList/TodoList.js
@@ -1,5 +1,87 @@
-const TodoList = () => {
-  return <h1>My Todo List</h1>;
+import { useState, useEffect } from "react";
+import { Button, CircularProgress, TextField } from "@mui/material";
+import Delete from "@mui/icons-material/Delete";
+import { getTodos, postTodo, deleteTodo } from "../../services/todosApi";
+
+const rowStyle = {
+  display: "flex",
+  alignItems: "center",
+  width: 250,
+  justifyContent: "space-between",
 };
 
-export default TodoList;
+const TodoListExample = () => {
+  const [isProcessing, setIsProcessing] = useState("loading");
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    async function fetchTodos() {
+      debugger;
+      let response = await getTodos();
+      setTodos(response);
+      setIsProcessing("done");
+    }
+
+    fetchTodos();
+  }, []);
+
+  const addTodo = async () => {
+    setIsProcessing("loading");
+    let response = await postTodo(newTodo);
+    setIsProcessing("done");
+    setTodos(response);
+    setNewTodo("");
+  };
+
+  const removeTodo = async (id) => {
+    setIsProcessing("loading");
+    let response = await deleteTodo(id);
+    setIsProcessing("done");
+    setTodos(response);
+  };
+
+  const handleTextFieldChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Todo List Example</h1>
+      {isProcessing === "loading" && <CircularProgress />}
+      {isProcessing === "done" && (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <div style={rowStyle}>
+                {todo.action}
+                <Button onClick={() => removeTodo(todo.id)} color="error">
+                  <Delete />
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+      <p />
+      <TextField
+        variant="outlined"
+        size="small"
+        onChange={handleTextFieldChange}
+        value={newTodo}
+        disabled={isProcessing === "loading"}
+      />
+      <p />
+      <Button
+        disabled={isProcessing === "loading" || newTodo === ""}
+        onClick={addTodo}
+        variant="contained"
+        color="primary"
+      >
+        Add Todo
+      </Button>
+    </>
+  );
+};
+
+export default TodoListExample;
